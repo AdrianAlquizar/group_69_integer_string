e635ba0cb0e3191eebf4d03f1e1713ac22c1ba07
almadendj <almadendj@gmail.com>
2024-10-19 18:59:35 +0800

    initial variable shit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let number: i32 = 5;
+    let string: String = "40".to_string();
+
+    let number_string: String = number.to_string();
+    let string_number: i32 = string.parse().unwrap();
+
     println!("Hello, world!");
 }
